d8e1f114358242aff349117b18a5aebdc03bfe06
Mounir El-Abbas <eamounir1@gmail.com>
2022-07-09 15:34:05 -0400

    Navigate_user_after_register_&_local_storage

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Logo, FormRow, Alert } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
 import { useAppContext } from "../context/appContext";
-
+import { useNavigate } from "react-router-dom";
 const initialState = {
   name: "",
   email: "",
@@ -11,10 +11,10 @@ const initialState = {
 };
 
 const Register = () => {
+  const navigate = useNavigate();
   const [values, setValues] = useState(initialState);
-  //global state and useNavigate
-
-  const { isLoading, showAlert, displayAlert, registerUser } = useAppContext();
+  const { user, isLoading, showAlert, displayAlert, registerUser } =
+    useAppContext();
 
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
@@ -38,6 +38,15 @@ const Register = () => {
       registerUser(currentUser);
     }
   };
+
+  useEffect(() => {
+    if (user) {
+      setTimeout(() => {
+        console.log("redirecting.....");
+        navigate("/");
+      }, 1000);
+    }
+  }, [user, navigate]);
 
   return (
     <Wrapper className="full-page">
