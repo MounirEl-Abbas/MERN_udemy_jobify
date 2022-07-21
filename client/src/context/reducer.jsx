8a200684549ddde7119ddc84f071ed1798fc8f30
Mounir El-Abbas <eamounir1@gmail.com>
2022-07-21 11:24:32 -0400

    stats-page/front&back/aggregation

--- client/src/context/reducer.jsx
+++ client/src/context/reducer.jsx
@@ -21,6 +21,8 @@ import {
   EDIT_JOB_BEGIN,
   EDIT_JOB_SUCCESS,
   EDIT_JOB_ERROR,
+  SHOW_STATS_BEGIN,
+  SHOW_STATS_SUCCESS,
 } from "./actions";
 import { initialState } from "./appContext";
 
@@ -222,7 +224,7 @@ const reducer = (state, action) => {
   if (action.type === EDIT_JOB_BEGIN) {
     return {
       ...state,
-      isLoading: false,
+      isLoading: true,
     };
   }
   if (action.type === EDIT_JOB_SUCCESS) {
@@ -241,6 +243,24 @@ const reducer = (state, action) => {
       showAlert: true,
       alertType: "danger",
       alertText: action.payload.msg,
+    };
+  }
+
+  /* SHOW STATS */
+
+  if (action.type === SHOW_STATS_BEGIN) {
+    return {
+      ...state,
+      isLoading: true,
+      showAlert: false,
+    };
+  }
+  if (action.type === SHOW_STATS_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      stats: action.payload.stats,
+      monthlyApplications: action.payload.monthlyApplications,
     };
   }
 
--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -2,6 +2,7 @@ import Job from "../models/Job.js";
 import { StatusCodes } from "http-status-codes";
 import { BadRequestError, NotFoundError } from "../errors/index.js";
 import checkPermissions from "../utils/checkPermissions.js";
+import mongoose from "mongoose";
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -54,7 +55,64 @@ const deleteJob = async (req, res) => {
 };
 
 const showStats = async (req, res) => {
-  res.send("show stats job");
+  /* AGGREGATION --- https://mongoosejs.com/docs/api/aggregate.html */
+
+  //aggregation is a series of steps to effectively engineer your data
+  //$match step: out of all the jobs in DB, match the jobs created by : id given
+  //Now that we have all the jobs created by the user that is logged in...
+  //we wanna group them by status, for the sake of the "stats" page
+
+  //$group step : group them by status
+  //the _id is mongoose syntax
+  //the count :{sum:1} is also syntax to count the number of times the status is present
+  //result:
+  /* {
+    "stats": [
+        {
+            "_id": "declined",
+            "count": 31
+        },
+        {
+            "_id": "interview",
+            "count": 15
+        },
+        {
+            "_id": "pending",
+            "count": 29
+        }
+    ]
+}
+   */
+
+  let stats = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    { $group: { _id: "$status", count: { $sum: 1 } } },
+  ]);
+
+  // turn result into an object, easy to manipulate in front-end
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
+
+  /* {
+    "stats": {
+        "declined": 31,
+        "interview": 15,
+        "pending": 29
+    }
+} */
+
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  };
+
+  let monthlyApplications = [];
+
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
 
 export { createJob, getAllJobs, deleteJob, updateJob, showStats };
